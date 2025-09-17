28462e8e741ef025b0a36044c6df625bf7b4ed58
ClaudioDev <aleledesma687@gmail.com>
2025-09-17 00:53:12 -0300

    Implement form automation on Text Box page with Selenium

--- main.py
+++ main.py
@@ -19,10 +19,32 @@ username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_locate
 password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
 login_button = driver.find_element(By.ID, 'login')
 
-# Fill in username and password
+# Fill in username and password, and clock the button
 username_field.send_keys('pythonstudent')
 password_field.send_keys('PythonStudent123$')
 driver.execute_script("arguments[0].click();", login_button)
+
+# Locate the Elements dropdown and Test Box
+elements = (WebDriverWait(driver, 10).
+            until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div/div[1]'))))
+elements.click()
+text_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0')))
+text_box.click()
+
+# Locate the from fields and submitt button
+fullname_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+email_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userEmail')))
+current_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'currentAddress')))
+permanent_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'permanentAddress')))
+submit_button = driver.find_element(By.ID, 'submit')
+
+
+# Fill in the form fields
+fullname_field.send_keys('Automation Test')
+email_field.send_keys('automation@gmail.com')
+current_address_field.send_keys('123 Automation St, Test City, TX')
+permanent_address_field.send_keys('456 Permanent Ave, Test City, TX')
+driver.execute_script("arguments[0].click();", submit_button)
 
 input("Press Enter to close the browser")
 driver.quit()
